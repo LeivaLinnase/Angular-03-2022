15dbb6a26fbd8fe16dbab4e7e713abac042f9bdf
LeivaLinnase <102084848+LeivaLinnase@users.noreply.github.com>
2022-04-14 12:17:00 +0300

    uusProject

---
+++ webshop/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './admin/add-product/add-product.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { EditProductComponent } from './admin/edit-product/edit-product.component';
+import { ViewProductComponent } from './admin/view-product/view-product.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "ostukorv", component: CartComponent},
+  { path: "admin", component: AdminHomeComponent},
+  { path: "admin/lisa", component: AddProductComponent},
+  { path: "admin/muuda", component: EditProductComponent},
+  { path: "admin/halda", component: ViewProductComponent},
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ webshop/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { AddProductComponent } from './admin/add-product/add-product.component';
+import { EditProductComponent } from './admin/edit-product/edit-product.component';
+import { ViewProductComponent } from './admin/view-product/view-product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    AdminHomeComponent,
+    AddProductComponent,
+    EditProductComponent,
+    ViewProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

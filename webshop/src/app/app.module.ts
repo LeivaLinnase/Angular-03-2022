9fc70f20756036ec8795fbe5e35569344bf1f328
LeivaLinnase <102084848+LeivaLinnase@users.noreply.github.com>
2022-04-19 12:20:00 +0300

    bootstrap/navbar, firebase/database

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 import { AddProductComponent } from './admin/add-product/add-product.component';
 import { EditProductComponent } from './admin/edit-product/edit-product.component';
 import { ViewProductComponent } from './admin/view-product/view-product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +25,30 @@ import { ViewProductComponent } from './admin/view-product/view-product.componen
     AdminHomeComponent,
     AddProductComponent,
     EditProductComponent,
-    ViewProductComponent
+    ViewProductComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserModule,
+        // ngx-translate and the loader module
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
